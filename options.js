c5eb5da2fc2133a78e51aaee6ab6146072cfb7bc
YongHyuk <tjdydgur123@gmail.com>
2021-05-01 15:46:55 -0700

    edge case for query input + save into cache

--- options.js
+++ options.js
@@ -1,15 +1,26 @@
 var submitButton = document.getElementById("submit");
 var addedSiteList = new Array();
+var addedQueryList = new Array();
 
 var counter = 0;
 submitButton.onclick = function () {
   var addedSite = document.getElementById("siteInput").value;
+  var addedQuery = document.getElementById("queryInput").value;
+  console.log(addedSite);
+  console.log(addedQuery);
   if (addedSite.length == 0) {
     return;
+  } else {
+    addedSiteList.push(addedSite);
+    cacheSites(addedSiteList);
+    addItem();
   }
-  addedSiteList.push(addedSite);
-  cacheSites(addedSiteList);
-  addItem();
+  if (addedQuery.length == 0) {
+    return;
+  } else {
+    addedQueryList.push(addedQuery);
+    cacheQuery(addedQueryList);
+  }
 };
 
 function addItem() {
@@ -27,4 +38,8 @@ function cacheSites(site) {
   chrome.storage.sync.get(/* String or Array */ ["sites"], function (item) {
     alert(item.sites);
   });
+}
+
+function cacheQuery(query) {
+  chrome.storage.sync.set({ queries: query }, function () {});
 }
