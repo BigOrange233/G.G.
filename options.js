86157b8f58bfe63b148ece974d86069a989998e1
Andrew Ursu <aursu@sfu.ca>
2021-05-01 12:05:42 -0700

    Added cache

--- options.js
+++ options.js
@@ -1,34 +1,19 @@
-// let page = document.getElementById('buttonDiv');
-// const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-// function constructOptions(kButtonColors) {
-//   for (let item of kButtonColors) {
-//     let button = document.createElement('button');
-//     button.style.backgroundColor = item;
-//     button.addEventListener('click', function() {
-//       chrome.storage.sync.set({color: item}, function() {
-//         console.log('color is ' + item);
-//       })
-//     });
-//     page.appendChild(button);
-//   }
-// }
-// constructOptions(kButtonColors);
 
 var submitButton = document.getElementById('submit');
 
-var blockedSiteList = new Array("Andrew");
+var addedSiteList = new Array();
 
 submitButton.onclick = function() {
   var addedSite = document.getElementById('siteInput').value
-  blockedSiteList.push(addedSite)
-  alert(blockedSiteList)
-  alert(addedSite)
+  addedSiteList.push(addedSite)
+  cacheSites(addedSiteList)
 };
 
 
 function cacheSites(site) {
-  chrome.runtime.onInstalled.addListener(function() {
+  localStorage.setItem("sites", JSON.stringify(addedSiteList));
 
-    
-  });
+  var siteList = JSON.parse(localStorage.getItem("sites"));
+  alert(siteList)
+
 }
