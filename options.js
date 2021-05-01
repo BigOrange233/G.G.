a9bc1854053074095fc423342d5f5ee49f49ad7f
YongHyuk <tjdydgur123@gmail.com>
2021-05-01 15:21:09 -0700

    edge case for empty input

--- options.js
+++ options.js
@@ -1,39 +1,30 @@
-
-var submitButton = document.getElementById('submit');
+var submitButton = document.getElementById("submit");
 var addedSiteList = new Array();
 
-var counter= 0;
-submitButton.onclick = function() {
-  var addedSite = document.getElementById('siteInput').value
-  addedSiteList.push(addedSite)
-  cacheSites(addedSiteList)
-  addItem()
+var counter = 0;
+submitButton.onclick = function () {
+  var addedSite = document.getElementById("siteInput").value;
+  if (addedSite.length == 0) {
+    return;
+  }
+  addedSiteList.push(addedSite);
+  cacheSites(addedSiteList);
+  addItem();
 };
 
-
-
-
-
-function addItem(){
+function addItem() {
   var ul = document.getElementById("dynamic-list");
   var candidate = document.getElementById("siteInput");
   var li = document.createElement("li");
-  li.setAttribute('id',candidate.value);
+  li.setAttribute("id", candidate.value);
   li.appendChild(document.createTextNode(candidate.value));
   ul.appendChild(li);
 }
 
-
-
 function cacheSites(site) {
-  chrome.storage.sync.set({ "sites": site }, function(){});
+  chrome.storage.sync.set({ sites: site }, function () {});
 
-  chrome.storage.sync.get(/* String or Array */["sites"], function(item){
-  
+  chrome.storage.sync.get(/* String or Array */ ["sites"], function (item) {
     alert(item.sites);
   });
-
-
 }
-
-
